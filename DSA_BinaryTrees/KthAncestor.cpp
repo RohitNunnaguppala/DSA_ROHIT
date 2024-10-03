fdadff0f2f3ef37de11bd8730e2370725528393a
RohitNunnaguppala <151500766+RohitNunnaguppala@users.noreply.github.com>
2024-10-03 23:57:47 +0530

    Add files via upload

---
+++ DSA_BinaryTrees/KthAncestor.cpp
@@ -0,0 +1,41 @@
+node* solve(node*root,int &k,int node){
+    if(root==null){
+        return null;
+    }
+    if(root->data==node){
+        return root;
+    }
+
+    node* leftans=solve(root->left,k,node)
+    node* rightans=solve(root->right,k,node)
+
+    if(leftans != nullptr && rightans == nullptr){
+        k--;
+        if(k<=0){
+            k=INT_MAX;
+            return root;
+        }
+        return leftans;
+    }
+
+    if(leftans == nullptr && rightans != nullptr){
+        k--;
+        if(k<=0){
+            k=INT_MAX;
+            return root;
+        }
+        return rightans;
+    }
+    return nullptr;
+
+}
+
+int kthancestor(node* root,int k,int node){
+    node* ans=solve(root,k,node);
+    if(ans==nullptr || ans->Data==node){
+        return -1;
+    }
+    else{
+        return ans->data;
+    }
+}
