41aca1d32d0ff353b59e318c704e99f33a5810ed
RohitNunnaguppala <nunnaguppalarohit@gmail.com>
2024-10-22 19:57:48 +0530

    DP

--- DSA_DP/8_GridUniquePath.cpp
+++ DSA_DP/8_GridUniquePath.cpp
@@ -1,3 +1,43 @@
+//Mem
+int countwaysutil(int i,int j,vector<vector<int>>& dp){
+    if(i==0&&j==0) return 1;
+    if(i<0||j<0) return 0;
+    if(dp[i][j]!=-1){
+        return dp[i][j];
+    }
+    int up=countwaysutil(i-1,j,dp);
+    int left=countwaysutil(i,j-1,dp);
+    return dp[i][j]=up+left;
+}
+int countways(int m,int n){
+    vector<vector<int>> dp(m,vector<int>(n,-1));
+    return countwaysutil(m-1,n-1,dp);
+}
+//tab
+int countWaysUtil(int m, int n, vector<vector<int>>& dp) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == 0 && j == 0) {
+                dp[i][j] = 1;
+                continue;
+            }
+            int up = 0;
+            int left = 0;
+            if (i > 0)
+                up = dp[i - 1][j];
+            if (j > 0)
+                left = dp[i][j - 1];
+
+            dp[i][j] = up + left;
+        }
+    }
+    return dp[m - 1][n - 1];
+}
+int countways(int m,int n){
+    vector<vector<int>> dp(m,vector<int>(n,-1));
+    return countwaysutil(m,n,dp);
+}
+//space
 class Solution {
 public:
     int uniquePaths(int m, int n) {
