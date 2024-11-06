a8c9eb407ee5854f062720694f02355ec89fcc30
RohitNunnaguppala <nunnaguppalarohit@gmail.com>
2024-11-06 23:19:15 +0530

    DP

---
+++ Leetcode/459.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        string str = s + s;
+        str = str.substr(1, str.size() - 2);
+        return str.find(s) != string::npos;
+    }
+};
