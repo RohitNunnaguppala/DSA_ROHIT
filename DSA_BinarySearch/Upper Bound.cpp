842ec264ee847a863a63f1a6ba0d2cb2bc45b122
RohitNunnaguppala <nunnaguppalarohit@gmail.com>
2025-01-07 22:31:12 +0530

    upperbound

---
+++ DSA_BinarySearch/Upper Bound.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int upperbound(vector<int> arr,int n,int x){
+    int low=0;
+    int high=n-1;
+    int ans=n;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(arr[mid]>x){
+            ans=mid;
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    vector<int> arr = {3, 5, 8, 15, 19};
+    int n = 5, x = 9;
+    int ind =upperbound(arr, n, x);
+    cout << "The lower bound is the index: " << ind << "\n";
+    return 0;
+}
