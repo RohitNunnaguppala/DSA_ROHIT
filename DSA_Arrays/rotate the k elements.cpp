39f32699edfbcde15ba29a8d897c14420759f081
RohitNunnaguppala <nunnaguppalarohit@gmail.com>
2025-01-21 13:39:30 +0530

    rotate by k

--- DSA_Arrays/rotate the k elements.cpp
+++ DSA_Arrays/rotate the k elements.cpp
@@ -20,3 +20,35 @@ int main(){
         cout<<res[i]<<" ";
     }
 }
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void reverse(vector<int> &arr,int l,int r){
+    while(l<r){
+        swap(arr[l],arr[r]);
+        l++;
+        r--;
+    }
+}
+void rotate(vector<int>& nums,int k) {
+        int n=nums.size();
+        k=k%n;
+        if(k==0){
+            return;
+        }
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
+}
+int main() {
+    vector<int> nums={1, 2, 3, 4, 5};
+    int k=2;
+    rotate(nums,k);
+     for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
