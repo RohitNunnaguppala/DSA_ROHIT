f5291fe022ba01a8d939e5760f8d047e8831d8d4
RohitNunnaguppala <nunnaguppalarohit@gmail.com>
2025-02-08 23:19:40 +0530

    Graph Representation

---
+++ DSA_Graphs/Graph Representation.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    vector<int> adj[n];
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    for(int i = 0; i < n; i++) {
+        cout << "Node " << i << ": ";
+        for(int neighbor : adj[i]) {
+            cout << neighbor << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
