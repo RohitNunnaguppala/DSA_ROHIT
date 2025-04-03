6182933590b4627b39fb6d63e13e19e40885fc93
RohitNunnaguppala <nunnaguppalarohit@gmail.com>
2025-04-03 22:22:06 +0530

    LEFTVIEW

--- DSA_BinaryTrees/LeftView.cpp
+++ DSA_BinaryTrees/LeftView.cpp
@@ -32,3 +32,19 @@ vector<int> leftView(Node *root)
    return ans;
 >>>>>>> fdadff0f2f3ef37de11bd8730e2370725528393a
 }
+void solve(Node* root, vector<int> &ans, int level) {
+    if(root == NULL)
+        return ;    
+    if(level == ans.size())
+        ans.push_back(root->data);
+        
+    solve(root->left, ans, level+1);
+    solve(root->right, ans, level+1);
+}
+
+vector<int> leftView(Node *root)
+{
+   vector<int> ans;
+   solve(root, ans, 0);
+   return ans;
+}
