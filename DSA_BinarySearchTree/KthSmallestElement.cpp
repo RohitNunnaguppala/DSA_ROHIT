d768ad16137b022ba26d757fa7890fed950fde39
RohitNunnaguppala <nunnaguppalarohit@gmail.com>
2025-04-12 22:41:33 +0530

    KthSmallestElement

--- DSA_BinarySearchTree/KthSmallestElement.cpp
+++ DSA_BinarySearchTree/KthSmallestElement.cpp
@@ -16,4 +16,5 @@ int solve(TreeNode* root, int& i, int k) {
     int kthSmallest(TreeNode* root, int k) {
         int i = 0; 
         return solve(root, i, k);
-    }
+}
+// This function finds the kth smallest element in a binary search tree (BST) using an in-order traversal. It uses a helper function `solve` that performs the traversal and keeps track of the count of nodes visited. When the count matches k, it returns the value of that node. The main function initializes the count and calls the helper function.
