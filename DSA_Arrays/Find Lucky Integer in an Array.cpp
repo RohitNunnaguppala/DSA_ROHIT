1bdb23065f0c66ad93d2c0a894f15c4057713484
RohitNunnaguppala <nunnaguppalarohit@gmail.com>
2025-04-30 23:06:24 +0530

    lucky interger

--- DSA_Arrays/Find Lucky Integer in an Array.cpp
+++ DSA_Arrays/Find Lucky Integer in an Array.cpp
@@ -1,14 +1,15 @@
 class Solution {
     public:
+
         int findLucky(vector<int>& arr) {
-            unordered_map<int, int> freq;
+            unordered_map<int, int> fre;
             for (int n:arr) {
-                freq[n]++;
+                fre[n]++;
             }
     
             int res=-1;
     
-            for(auto& [n, cnt]:freq) {
+            for(auto& [n, cnt]:fre) {
                 if(n==cnt) {
                     res=max(res,n); 
                 }
