a98d08a082ff7e1de47e9f5567e2c708ac0731bc
RohitNunnaguppala <nunnaguppalarohit@gmail.com>
2025-05-27 12:33:02 +0530

    Partition Set Into 2 Subsets With Min Absolute Sum Diff

--- DSA_DP/Partition Set Into 2 Subsets With Min Absolute Sum Diff.cpp
+++ DSA_DP/Partition Set Into 2 Subsets With Min Absolute Sum Diff.cpp
@@ -37,4 +37,52 @@ public:
 };
 
 
-//space
+//tabulation
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int minSubsetSumDifference(vector<int>& arr, int n) {
+    int totsum = 0;
+    for (int i = 0; i < n; i++) totsum += arr[i];
+
+    vector<vector<bool>> dp(n, vector<bool>(totsum + 1, false));
+
+    // Base case: Subset sum of 0 is always possible
+    for (int i = 0; i < n; i++) dp[i][0] = true;
+
+    // Base case: First element alone
+    if (arr[0] <= totsum) dp[0][arr[0]] = true;
+
+    // Tabulation
+    for (int ind = 1; ind < n; ind++) {
+        for (int target = 1; target <= totsum; target++) {
+            bool notTaken = dp[ind - 1][target];
+            bool taken = false;
+            if (target >= arr[ind])
+                taken = dp[ind - 1][target - arr[ind]];
+            dp[ind][target] = notTaken || taken;
+        }
+    }
+
+    // Find the minimum absolute difference
+    int mini = 1e9;
+    for (int s1 = 0; s1 <= totsum / 2; s1++) {
+        if (dp[n - 1][s1] == true) {
+            int s2 = totsum - s1;
+            mini = min(mini, abs(s2 - s1));
+        }
+    }
+
+    return mini;
+}
+
+int main() {
+    vector<int> arr = {1, 2, 3, 4};
+    int n = arr.size();
+    cout << "The minimum absolute difference is: " << minSubsetSumDifference(arr, n);
+    return 0;
+}
+
+// Space-optimization
+
