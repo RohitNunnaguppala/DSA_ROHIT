69181c22cd99e1b159a4ed14816a13f8034fa66a
RohitNunnaguppala <nunnaguppalarohit@gmail.com>
2025-05-27 08:45:32 +0530

    house robbe₹II

--- DSA_DP/6_roberHouse2.cpp
+++ DSA_DP/6_roberHouse2.cpp
@@ -1,33 +1,40 @@
-class Solution {
-public:
-    int solve(vector<int>& nums){
-        int n=nums.size();
-        long long int prev2=0;
-        long long int prev=nums[0];
 
-        for(int i=1;i<n;i++){
-            long long int pick=nums[i];
-            if(i>=2){
-                pick+=prev2;
-            }
-            long long int nonpick=0+prev;
-            long long int cur_i=max(pick,nonpick);
-            prev2=prev;
-            prev=cur_i;
-        }
-        return prev;
-    }
-    int rob(vector<int>& nums) {
-        vector<int> temp1,temp2;
-        int n=nums.size();
-        if(n==1) return nums[0];
-        for(int i=0;i<n;i++){
-            if(i!=0) temp1.push_back(nums[i]);
-            if(i!=n-1) temp2.push_back(nums[i]);
-        }
-    long long int ans1 = solve(temp1);
-    long long int ans2 = solve(temp2);
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(int n, vector<int>& arr) {
+    int prev=arr[0];
+    int prev1=0;
     
-    return max(ans1,ans2);
+    for(int i=1;i<n;i++){
+      int pick=arr[i];
+      if(i>1) pick+=prev1;
+      
+      int nonpick=0+prev;
+      
+      int curr=max(pick,nonpick);
+      prev1=prev;
+      prev=curr;
     }
-};
+    return prev;
+}
+
+long long int house(int n,vector<int> arr){
+  vector<int> temp1,temp2;
+  
+  for(int i=0;i<n;i++){
+    if(i!=0) temp1.push_back(arr[i]);
+    if(i!=n-1) temp2.push_back(arr[i]);
+  }
+  
+  return max(solve(n,temp1),solve(n,temp2));
+}
+int main() {
+    vector<int> arr{1,5,1,2,6};
+    int n = arr.size();
+    
+    cout << house(n, arr);
+
+    return 0;
+}
+
