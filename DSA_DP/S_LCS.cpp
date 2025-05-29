9ccaed840c0b74abf89d93c34e0a34e681fea221
RohitNunnaguppala <nunnaguppalarohit@gmail.com>
2025-05-29 09:54:50 +0530

    lcs

--- DSA_DP/S_LCS.cpp
+++ DSA_DP/S_LCS.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    int lcsUtil(string& s1, string& s2, int ind1, int ind2, vector<vector<int>>& dp) {
-        if (ind1 < 0 || ind2 < 0)
+    int lcs(string& s1, string& s2, int ind1, int ind2, vector<vector<int>>& dp) {
+        if(ind1<0||ind2<0){
             return 0;
+        }
 
-        if (dp[ind1][ind2] != -1)
-            return dp[ind1][ind2];
+        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
 
-        if (s1[ind1] == s2[ind2])
-            return dp[ind1][ind2] = 1 + lcsUtil(s1, s2, ind1 - 1, ind2 - 1, dp);
-        else
-            return dp[ind1][ind2] = max(lcsUtil(s1, s2, ind1, ind2 - 1, dp), lcsUtil(s1, s2, ind1 - 1, ind2, dp));
+        if(s1[ind1]==s2[ind2]){
+            return dp[ind1][ind2]=1+lcs(s1,s2,ind1-1,ind2-1,dp);
+        }
+
+        return dp[ind1][ind2]=max(lcs(s1,s2,ind1-1,ind2,dp),lcs(s1,s2,ind1,ind2-1,dp));
     }
 
     int longestCommonSubsequence(string text1, string text2) {
@@ -18,6 +19,6 @@ public:
         int m = text2.size();
 
         vector<vector<int>> dp(n, vector<int>(m, -1)); 
-        return lcsUtil(text1, text2, n - 1, m - 1, dp);
+        return lcs(text1, text2, n - 1, m - 1,dp);
     }
 };
