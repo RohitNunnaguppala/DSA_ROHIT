7875ece3b38781f79f8a457cd18cb7fd62038109
RohitNunnaguppala <nunnaguppalarohit@gmail.com>
2025-05-29 18:39:06 +0530

    longest palindromic subseq

--- DSA_DP/S_LPS.cpp
+++ DSA_DP/S_LPS.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    int lcs(string s1, string s2) {
-        int n = s1.size();
-        int m = s2.size();
+    int lcsUtil(string& s1, string& s2, int ind1, int ind2, vector<vector<int>>& dp) {
+    if (ind1 < 0 || ind2 < 0)
+        return 0;
 
-        vector<int> prev(m + 1, 0), cur(m + 1, 0);
+    if (dp[ind1][ind2] != -1)
+        return dp[ind1][ind2];
 
-        for (int ind1 = 1; ind1 <= n; ind1++) {
-            for (int ind2 = 1; ind2 <= m; ind2++) {
-                if (s1[ind1 - 1] == s2[ind2 - 1])
-                    cur[ind2] = 1 + prev[ind2 - 1];
-                else
-                    cur[ind2] = max(prev[ind2], cur[ind2 - 1]);
-            }
-            prev = cur;
-        }
-
-        return prev[m];
+    if (s1[ind1] == s2[ind2])
+        return dp[ind1][ind2] = 1 + lcsUtil(s1, s2, ind1 - 1, ind2 - 1, dp);
+    else
+        return dp[ind1][ind2] = max(
+            lcsUtil(s1, s2, ind1 - 1, ind2, dp),
+            lcsUtil(s1, s2, ind1, ind2 - 1, dp)
+        );
     }
 
+    int lcs(string s1, string s2) {
+       int n = s1.size(), m = s2.size();
+       vector<vector<int>> dp(n, vector<int>(m, -1));
+       return lcsUtil(s1, s2, n - 1, m - 1, dp);
+    }
+
+
     int longestPalindromeSubseq(string s) {
-        string t = s;
-        reverse(s.begin(), s.end());
-        return lcs(s, t);
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+        return lcs(s, rev);  
     }
 };
