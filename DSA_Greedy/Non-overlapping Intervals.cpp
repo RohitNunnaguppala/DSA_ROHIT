9d76f7d9aaf036236eb9774327afc7105067fbbb
Rohit Nunnaguppala <nunnaguppalarohit@gmail.com>
2025-06-08 00:37:33 +0530

    Non-overlapping Intervals

---
+++ DSA_Greedy/Non-overlapping Intervals.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        if(intervals.size()==0) return 0;
+
+        sort(intervals.begin(),intervals.end(),[](vector<int> &a,vector<int> &b){
+            return a[1]<b[1];
+        });
+
+        int cnt=1;
+        int end=intervals[0][1];
+        for(int i=1;i<intervals.size();i++){
+            if(intervals[i][0]>=end){
+                cnt=cnt+1;
+                end=intervals[i][1];
+            }
+        }
+        return intervals.size()-cnt;
+    }
+};
